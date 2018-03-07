6073235a48c27a2dcfb173bd7703cadfa8870068
Daniel Levitt <daniellevitt86@gmail.com>
2018-03-07 16:00:18 +0000

    Added XO to the project

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const chalk = require("chalk");
-const meow = require("meow");
-const fs = require("fs");
-const findModules = require("find-node-modules");
+#!/usr/bin/env node
+const fs = require('fs');
+const chalk = require('chalk');
+const meow = require('meow');
+const findModules = require('find-node-modules');
 const logSymbols = require('log-symbols');
-const recast = require("recast");
-const R = require("ramda");
-const parser = require("./parser");
+const recast = require('recast');
+const R = require('ramda');
+const parser = require('./parser');
 
 const log = console.log;
 
@@ -23,54 +24,55 @@ const cli = meow(
   {
     flags: {
       library: {
-        type: "string",
-        alias: "l"
-      }
-    }
+        type: 'string',
+        alias: 'l',
+      },
+    },
   }
 );
 
 const errors = [];
 
-if (!cli.input.length) {
-  errors.push("Missing <input>");
+if (!cli.input.length > 0) {
+  errors.push('Missing <input>');
 }
 if (!cli.flags.library) {
-  errors.push("Missing --library option");
+  errors.push('Missing --library option');
 }
 
-if (errors.length) {
-  return log(`
-${chalk.red.bold("Error")}
+if (errors.length > 0) {
+  log(`
+${chalk.red.bold('Error')}
 
 ${logSymbols.error} ${R.join(`\n${logSymbols.error} `, errors)}
 
 ---------
 ${cli.help}`);
+  process.exit(1);
 }
 
 const sortAlpha = R.sort((a, b) => a.localeCompare(b));
 
 const getFns = R.pipe(
-  x => recast.parse(x, { parser }),
-  R.path(["program", "body"]),
-  R.filter(R.propEq("type", "ImportDeclaration")),
-  R.filter(R.pathEq(["source", "value"], cli.flags.library)),
-  R.map(R.pipe(R.prop("specifiers"), R.map(R.path(["local", "name"])))),
+  x => recast.parse(x, {parser}),
+  R.path(['program', 'body']),
+  R.filter(R.propEq('type', 'ImportDeclaration')),
+  R.filter(R.pathEq(['source', 'value'], cli.flags.library)),
+  R.map(R.pipe(R.prop('specifiers'), R.map(R.path(['local', 'name'])))),
   R.flatten
 );
 
 const getUsedFns = R.pipe(
-  R.map(file => fs.readFileSync(file, "utf-8")),
+  R.map(file => fs.readFileSync(file, 'utf-8')),
   R.map(getFns),
   R.flatten,
   R.uniq,
   sortAlpha
 );
 
-const modulesPath = findModules({ cwd: cli.input[0], relative: false });
+const modulesPath = findModules({cwd: cli.input[0], relative: false});
 
-const availableFns = R.keys(require(modulesPath[0] + "/" + cli.flags.library));
+const availableFns = R.keys(require(modulesPath[0] + '/' + cli.flags.library));
 
 const fns = getUsedFns(cli.input);
 
@@ -82,7 +84,7 @@ const percentage = R.pipe(R.divide(unusedFns.length), R.multiply(100), x =>
   x.toFixed(2)
 )(availableFns.length);
 
-let output = `
+const output = `
 ${chalk.bold(`Usage for '${cli.flags.library}'`)}
 ---------
 
@@ -91,10 +93,10 @@ ${logSymbols.info} Using ${chalk.bold(percentage)}% out of ${chalk.bold(
 )} available exports
 
 ${chalk.bold(`Used fns (${fns.length})`)}
-  - ${R.join(", ", fns)}
+  - ${R.join(', ', fns)}
 
 ${chalk.bold(`Unused fns (${unusedFns.length})`)}
-  - ${R.join(", ", unusedFns)}
+  - ${R.join(', ', unusedFns)}
 `;
 
 log(output);
--- parser.js
+++ parser.js
@@ -1,15 +1,3 @@
-/*
- *  Copyright (c) 2015-present, Facebook, Inc.
- *  All rights reserved.
- *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
- */
-
-'use strict';
-
 const babylon = require('babylon');
 
 // These are the options that were the default of the Babel5 parse function
@@ -34,9 +22,6 @@ const options = {
   ],
 };
 
-/**
- * Wrapper to set default options
- */
-exports.parse = function parse (code) {
+exports.parse = function(code) {
   return babylon.parse(code, options);
 };
